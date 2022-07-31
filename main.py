b4a75cbe1be0c012570c258db71aabda3be9e718
felipesilva543 <ffelipes543@gmail.com>
2022-07-31 18:26:27 -0300

    mudança da posição da chamada da operação

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ class calculadoraSimples:
     def __init__(self):
         self.n1 = 0
         self.n2 = 0
+        self.opc = 0
+        self.qtdItensMenu = 0
 
     def Iniciar(self):
         menu(0, 'CALCULADORA SIMPLES')
         while True:
             try:
                 self.n1 = int(input('Digite o primeiro valor: '))
+                self.opcoes()
+                if self.opc == 5:
+                    self.operação()
+                    break
                 while True:
                     try:
                         self.n2 = int(input('Digite o segundo valor: '))
@@ -21,37 +27,51 @@ class calculadoraSimples:
                 break        
             except:
                 print('Digite um valor inteiro!')
+        self.operação()
 
+    def opcoes(self):
+        self.qtdItensMenu = menu(1, 'Soma', 'Subtração', 'Multiplicação', 'Divisão', 'Raiz Quadrada')
         while True:
-            qtd = menu(1, 'Soma', 'Subtração', 'Multiplicação', 'Divisão', 'Raiz Quadrada')
-            while True:
-                try:
-                    res = int(input('Opção: '))
-                    break
-                except:
-                    print('Digite um valor inteiro!')
-            if 0 < res <= qtd+1:
-                if res == 1:
+            try:
+                self.opc = int(input('Opção: '))
+                break
+            except:
+                print('Digite um valor inteiro!')
+
+    def operação(self):
+        while True:
+            if 0 < self.opc <= self.qtdItensMenu+1:
+                if self.opc == 1:
                     print(f'Soma: {operacoes.som(self.n1, self.n2)}')
-                if res == 2:
+                if self.opc == 2:
                     print(f'Subtração: {operacoes.sub(self.n1, self.n2)}')
-                if res == 3:
+                if self.opc == 3:
                     print(f'Multiplicação: {operacoes.mul(self.n1, self.n2)}')
-                if res == 4:
+                if self.opc == 4:
                     print(f'Divisão: {operacoes.div(self.n1, self.n2)}')
-                if res == 5:
-                    t2 = menu(0, 'RAIZ DE:', str(self.n1), str(self.n2))
-                    while True:
+                if self.opc == 5:
+                    #t2 = menu(0, 'RAIZ DE:', str(self.n1), str(self.n2))
+                    print(f'Raiz Quarada: {operacoes.sqrt(self.n1 if op == 1 else self.n2)}')
+                    return
+                    """while True:
                         op = int(input('OPÇÃO: '))
                         if 0 < op <= t2:
                             print(f'Raiz Quarada: {operacoes.sqrt(self.n1 if op == 1 else self.n2)}')
                             break
                         else: 
-                            print('Valor incorreto!')
+                            print('Valor incorreto!')"""
             op = input('Fazer outra operação? [S/N]: ').capitalize().strip()
             if op[0] in 'Nn':
                 print('Até logo!')
                 break
+            else:
+                self.qtdItensMenu= menu(1, 'Soma', 'Subtração', 'Multiplicação', 'Divisão', 'Raiz Quadrada')
+            while True:
+                try:
+                    self.opc = int(input('Opção: '))
+                    break
+                except:
+                    print('Digite um valor inteiro!')
 
 calc1 = calculadoraSimples()
 calc1.Iniciar()
